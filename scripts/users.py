0cb1e180cc331288853ec48ee747a4714030e801
JeongWon Been <congcoding@gmail.com>
2026-03-12 20:04:49 +0900

    Create users.py

---
+++ scripts/users.py
@@ -0,0 +1,12 @@
+USER_MAP = {
+    "congcoding": "빈정원",
+    "Cjunhyun": "최준현",
+    "woody6624": "김우현",
+    "Floodnut": "정금종",
+    "HeejuKo": "고희주",
+    "PokingTeemo": "정윤섭",
+    "ddhi7": "원동희",
+    "MarkSon-42": "손민우",
+    "jiu-jung": "정지우",
+    "github-actions[bot]": "cheer-bot",
+}
